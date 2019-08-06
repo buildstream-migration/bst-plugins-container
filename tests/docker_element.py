9d199b62c36422d16734276f64890180e3e354a5
Shahwat Dalal <sdalal29@bloomberg.net>
2019-08-06 17:18:54 +0100

    tests: Move utilty methods into utils.py

--- tests/docker_element.py
+++ tests/docker_element.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-import hashlib
 import os
 import pytest
 import tarfile
 
-READ_WRITE_USER_PERMISSION = 0o755
+from tests.utils import build_and_checkout, untar, load_image, get_image_tag, hash_digest
+
+READ_WRITE_USER_PERMISSION = 0o775
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     'project'
@@ -17,18 +18,18 @@ def test_correct_checksum_docker_image(cli, datafiles, tmp_path):
     project = str(datafiles)
     checkout_dir = os.path.join(str(tmp_path), 'checkout')
 
-    _build_and_checkout(test_element, checkout_dir, cli, project)
-    extract_path = _untar(checkout_dir)
+    build_and_checkout(test_element, checkout_dir, cli, project)
+    extract_path = untar(checkout_dir)
 
     # check config file is correctly named
     config_json = [file for file in os.listdir(extract_path)
                    if file.endswith(".json") and file != "manifest.json"][0]
-    assert config_json == "{}.json".format(_hash_digest(os.path.join(extract_path, config_json)))
+    assert config_json == "{}.json".format(hash_digest(os.path.join(extract_path, config_json)))
 
     # check each directory is correctly named
     for layer in os.listdir(extract_path):
         if os.path.isdir(layer):
-            assert os.path.basename(layer) == _hash_digest(os.path.join(layer, 'layer.tar'))
+            assert os.path.basename(layer) == hash_digest(os.path.join(layer, 'layer.tar'))
 
 
 @pytest.mark.docker
@@ -42,8 +43,8 @@ def test_single_build_dep_docker_image(cli, docker_client, datafiles, tmp_path):
     # need to reset permission as copying files into `DATA_DIR` changes permissions
     os.chmod(hello_exec, READ_WRITE_USER_PERMISSION)
 
-    _build_and_checkout(test_element, checkout_dir, cli, project)
-    tag = _load_image(docker_client, checkout_dir)
+    build_and_checkout(test_element, checkout_dir, cli, project)
+    tag = get_image_tag(load_image(docker_client, checkout_dir))
 
     # compare output of docker run
     output = docker_client.containers.run(tag).decode('utf-8')
@@ -60,13 +61,13 @@ def test_multiple_deps_docker_image(docker_client, cli, datafiles, tmp_path):
     project = str(datafiles)
     checkout_dir = os.path.join(str(tmp_path), 'checkout')
 
-    _build_and_checkout(test_element, checkout_dir, cli, project)
-    tag = _load_image(docker_client, checkout_dir)
+    build_and_checkout(test_element, checkout_dir, cli, project)
+    tag = get_image_tag(load_image(docker_client, checkout_dir))
     _check_meta_data(docker_client, tag)
 
     image_attrs = docker_client.images.get(tag).attrs
     assert len(image_attrs['RootFS']['Layers']) == 3
-    _test_no_file_duplication(_get_layer_files(_untar(checkout_dir)))
+    _test_no_file_duplication(_get_layer_files(untar(checkout_dir)))
 
 
 @pytest.mark.docker
@@ -76,13 +77,13 @@ def test_nested_deps_docker_image(docker_client, cli, datafiles, tmp_path):
     project = str(datafiles)
     checkout_dir = os.path.join(str(tmp_path), 'checkout')
 
-    _build_and_checkout(test_element, checkout_dir, cli, project)
-    tag = _load_image(docker_client, checkout_dir)
+    build_and_checkout(test_element, checkout_dir, cli, project)
+    tag = get_image_tag(load_image(docker_client, checkout_dir))
     _check_meta_data(docker_client, tag)
 
     image_attrs = docker_client.images.get(tag).attrs
     assert len(image_attrs['RootFS']['Layers']) == 2
-    _test_no_file_duplication(_get_layer_files(_untar(checkout_dir)))
+    _test_no_file_duplication(_get_layer_files(untar(checkout_dir)))
 
 
 @pytest.mark.docker
@@ -92,15 +93,15 @@ def test_diamond_deps_docker_image(docker_client, cli, datafiles, tmp_path):
     project = str(datafiles)
     checkout_dir = os.path.join(str(tmp_path), 'checkout')
 
-    _build_and_checkout(test_element, checkout_dir, cli, project)
-    tag = _load_image(docker_client, checkout_dir)
+    build_and_checkout(test_element, checkout_dir, cli, project)
+    tag = get_image_tag(load_image(docker_client, checkout_dir))
     _check_meta_data(docker_client, tag)
 
     image_attrs = docker_client.images.get(tag).attrs
     assert len(image_attrs['RootFS']['Layers']) == 2
 
     # assert that there is no file duplication
-    layer_files = _get_layer_files(_untar(checkout_dir))
+    layer_files = _get_layer_files(untar(checkout_dir))
     _test_no_file_duplication(layer_files)
 
 
@@ -114,15 +115,15 @@ def test_nested_overwrite_docker_image(docker_client, cli, datafiles, tmp_path):
     exported_tar = os.path.join(container_fs_export_dir, 'image.tar')
     os.makedirs(container_fs_export_dir)
 
-    _build_and_checkout(test_element, checkout_dir, cli, project)
-    tag = _load_image(docker_client, checkout_dir)
+    build_and_checkout(test_element, checkout_dir, cli, project)
+    tag = get_image_tag(load_image(docker_client, checkout_dir))
     _check_meta_data(docker_client, tag)
 
     image_attrs = docker_client.images.get(tag).attrs
     assert len(image_attrs['RootFS']['Layers']) == 2
 
     # assert that file is indeed overwritten
-    extract_path = _untar(checkout_dir)
+    extract_path = untar(checkout_dir)
     assert _get_number_of_file_duplications(_get_layer_files(extract_path)) == 2
 
     # check overwritten file is content is as expected
@@ -165,11 +166,11 @@ def test_share_layers_docker_image(docker_client, cli, datafiles, tmp_path):
     checkout_dir1 = os.path.join(checkout_dir, 'test1')
     checkout_dir2 = os.path.join(checkout_dir, 'test2')
 
-    _build_and_checkout(test_element1, checkout_dir1, cli, project)
-    _build_and_checkout(test_element2, checkout_dir2, cli, project)
+    build_and_checkout(test_element1, checkout_dir1, cli, project)
+    build_and_checkout(test_element2, checkout_dir2, cli, project)
 
-    tag1 = _load_image(docker_client, checkout_dir1)
-    tag2 = _load_image(docker_client, checkout_dir2)
+    tag1 = get_image_tag(load_image(docker_client, checkout_dir1))
+    tag2 = get_image_tag(load_image(docker_client, checkout_dir2))
 
     fs1 = docker_client.images.get(tag1).attrs['RootFS']['Layers']
     fs2 = docker_client.images.get(tag2).attrs['RootFS']['Layers']
@@ -188,13 +189,6 @@ def _get_layer_files(extract_path):
     return [set([member.name for member in tar_info]) for tar_info in layer_files]
 
 
-def _untar(checkout_dir, artifact_name='image.tar'):
-    extract_path = os.path.join(os.path.dirname(checkout_dir), 'image_extract')
-    with tarfile.open(os.path.join(checkout_dir, artifact_name)) as tar_handle:
-        tar_handle.extractall(path=extract_path)
-    return extract_path
-
-
 def _get_number_of_file_duplications(layer_files):
     duplicated_files = set()
     for i, layer_a in enumerate(layer_files):
@@ -207,57 +201,9 @@ def _test_no_file_duplication(layer_files):
     assert _get_number_of_file_duplications(layer_files) == 0
 
 
-def _build_and_checkout(test_element, checkout_dir, cli, project):
-    # build image
-    result = cli.run(project=project, args=['build', test_element])
-    result.assert_success()
-    # checkout image
-    os.makedirs(checkout_dir)
-    result = cli.run(project=project, args=['artifact', 'checkout', '--directory', checkout_dir, test_element])
-    result.assert_success()
-
-
-def _load_image(docker_client, checkout_dir, artifact_name='image.tar'):
-    image_path = str(os.path.join(checkout_dir, artifact_name))
-    with open(image_path, 'rb') as image_handle:
-        response = docker_client.images.load(image_handle.read())
-        # get first tag of first image
-        tag = response[0].tags[0]
-    return tag
-
-
 def _check_meta_data(docker_client, tag):
     # check meta-data of image
     image_attrs = docker_client.images.get(tag).attrs
     date_created = datetime.strptime(image_attrs['Created'], '%Y-%m-%dT%H:%M:%SZ')
     assert date_created.date() == datetime.now().date()
     assert image_attrs['Author'] == 'BuildStream docker_image plugin'
-
-
-def _hash_digest(file):
-    """return hash digest of file
-
-    :param file: name of file to calculate hash of
-    :param algorithm: hash algorithm that wants to be used
-    :return: hash digest of specified file
-    """
-    hash_algorithm = hashlib.sha256()
-    with open(file, 'rb') as file_handle:
-        for block in _read_file_block(file_handle):
-            hash_algorithm.update(block)
-    return hash_algorithm.hexdigest()
-
-
-def _read_file_block(file_handle, block_size=8192):
-    """yield chunk_size blocks of file
-
-    :param file_handle: handle to file
-    :param chunk_size: block size of file to be read
-    :return: block of file
-    """
-    while True:
-        data = file_handle.read(block_size)
-        if not data:
-            break
-        else:
-            yield data
---
+++ tests/utils.py
@@ -0,0 +1,125 @@
+import hashlib
+import os
+import tarfile
+
+IMAGE_PREFIX = 'bst-plugins-container-tests'
+
+
+def push_image(docker_client, registry_url, image_name):
+    """
+    Pushes image_name to registry
+
+    :param docker_client: handle to Docker engine
+    :param registry_url: registry to push image to
+    :param image_name: name of image to be pushed
+    :return:
+    """
+    remote_tag = '{}/{}'.format(registry_url, image_name)
+    image = docker_client.images.get(image_name)
+    image.tag(remote_tag)
+    response = list(docker_client.images.push(remote_tag, stream=True, decode=True))
+    # check that image was pushed by parsing response logs
+    if "Pushed" not in [alert['status'] for alert in response if 'status' in alert.keys()]:
+        raise Exception("Image {} was not pushed to {}".format(image_name, registry_url))
+
+
+def untar(path, artifact_name='image.tar'):
+    """
+    Untars a tarball and returns extract path
+
+    :param path: directory of tarball
+    :param artifact_name: name of tarball
+    :return: path of where the tarball has been extracted to
+    """
+    extract_path = os.path.join(os.path.dirname(path), 'image_extract')
+    with tarfile.open(os.path.join(path, artifact_name)) as tar_handle:
+        tar_handle.extractall(path=extract_path)
+    return extract_path
+
+
+def build_and_checkout(test_element, checkout_dir, cli, project):
+    """
+    Builds and checkouts specified element
+
+    :param test_element: name of element to build and checkout
+    :param checkout_dir: directory of checkout
+    :param cli: handle to BuildStream
+    :param project: directory of project
+    """
+    # build image
+    result = cli.run(project=project, args=['build', test_element])
+    result.assert_success()
+    # checkout image
+    os.makedirs(checkout_dir)
+    result = cli.run(project=project, args=['artifact', 'checkout', '--directory', checkout_dir, test_element])
+    result.assert_success()
+
+
+def load_image(docker_client, path, artifact_name='image.tar'):
+    """
+    Loads a docker image to the Docker daemon. Equivalent to `docker load` command.
+
+    :param docker_client: handle to Docker engine
+    :param path: directory of image tarball
+    :param artifact_name: name of image tarball
+    :return: response from Docker daemon
+    """
+    image_path = str(os.path.join(path, artifact_name))
+    with open(image_path, 'rb') as image_handle:
+        response = docker_client.images.load(image_handle.read())
+        # get first tag of first image
+    return response
+
+
+def get_image_tag(response):
+    """
+    Parse response from Docker daemon to get tag of image.
+
+    :param response: response from Docker daemon
+    :return: Tag of image
+    """
+    return response[0].tags[0]
+
+
+def get_docker_host(docker_client):
+    """
+    Parse a DockerClient object to get the hostname of the Docker engine
+
+    :param docker_client: handle to Docker engine
+    :return: hostname of Docker engine
+    """
+    docker_host_url = docker_client.api.base_url.split('/')[-1]
+    if ':' in docker_host_url:
+        # strip port
+        return docker_host_url.split(':')[0]
+    else:
+        return docker_host_url
+
+
+def hash_digest(_file):
+    """return hash digest of file
+
+    :param _file: name of file to calculate hash of
+    :param algorithm: hash algorithm that wants to be used
+    :return: hash digest of specified file
+    """
+    hash_algorithm = hashlib.sha256()
+    with open(_file, 'rb') as file_handle:
+        for block in _read_file_block(file_handle):
+            hash_algorithm.update(block)
+    return hash_algorithm.hexdigest()
+
+
+def _read_file_block(file_handle, block_size=8192):
+    """yield chunk_size blocks of file
+
+    :param file_handle: handle to file
+    :param chunk_size: block size of file to be read
+    :return: block of file
+    """
+    while True:
+        data = file_handle.read(block_size)
+        if not data:
+            break
+        else:
+            yield data
