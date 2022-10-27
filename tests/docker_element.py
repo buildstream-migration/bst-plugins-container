50453c39e780436e9a09999fc88be28765cbdec5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 02:10:01 +0000

    Adding tarfile member sanitization to extractall()

--- tests/docker_element.py
+++ tests/docker_element.py
@@ -160,7 +160,26 @@ def test_nested_overwrite_docker_image(
         for chunk in container.export():
             tar_handle.write(chunk)
     with tarfile.open(exported_tar) as tar_handle:
-        tar_handle.extractall(path=container_fs_export_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar_handle, path=container_fs_export_dir)
 
     try:
         with open(
